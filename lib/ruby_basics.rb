91ae82b75528187d9431ec88897ecb55ce33336e
Katie Weber <k.weber0893@gmail.com>
2018-05-18 13:58:35 -0400

    Completed Ruby Basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num = num1 / num2
+ num
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
+def greeting(hello, friend)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping ="cheese")
+  topping
 end
